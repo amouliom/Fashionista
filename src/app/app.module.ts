6c6d9462ac8ca44c79d305930fd21efbb3692c86
Tunde Olu-Isa <your_email_address@example.com>
2020-11-18 01:32:36 -0600

    First Ever Commit Basic Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
